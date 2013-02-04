493d0335c9c112f43470c9af87c354e4280478c3
pdudits <pdudits@localhost>
2013-02-04 22:05:18 +0100

    Fixed imports in JOrbisPlayer

--- jorbis/player/JOrbisPlayer.java
+++ jorbis/player/JOrbisPlayer.java
@@ -36,6 +36,21 @@ import com.jcraft.jorbis.Block;
 import com.jcraft.jorbis.Comment;
 import com.jcraft.jorbis.DspState;
 import com.jcraft.jorbis.Info;
+import java.applet.AppletContext;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.util.Vector;
+import javax.sound.sampled.*;
+import javax.swing.*;
 
 public class JOrbisPlayer extends JApplet implements ActionListener, Runnable {
 
